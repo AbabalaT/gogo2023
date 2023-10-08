0aaf74b9eed1df3ab38c65933529c3a56cd0a1de
Kv <kv@qq.com>
2023-10-08 09:56:33 +0800

    01

---
+++ pcl2process/src/pcl2process.cpp
@@ -0,0 +1,394 @@
+//
+// Created by KevinTC.
+//
+#include <cmath>
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/voxel_grid.h>
+
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/features/normal_3d.h>
+
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
+
+//#include <pcl/features/normal_3d.h>
+//#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/surface/mls.h>
+
+tf2_ros::Buffer tfBuffer;
+ros::Publisher pcl_publisher;
+
+inline float float_abs(float x) {
+    if (x > 0) {
+        return x;
+    } else {
+        return -x;
+    }
+}
+
+struct PointInt {
+    int p_x, p_y, p_z;
+};
+
+float current_x = 0.0, current_y = 0.0;
+
+void getcloud_air(const sensor_msgs::PointCloud2ConstPtr &laserCloudMsg) {//法向量法投影转平面
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl2cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    sensor_msgs::PointCloud2 ROSPCL_output;
+    pcl::fromROSMsg(*laserCloudMsg, *pcl2cloud);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl2cloud_out(new pcl::PointCloud<pcl::PointXYZ>);
+
+    geometry_msgs::TransformStamped base2map;
+    try {
+        base2map = tfBuffer.lookupTransform("map", "base_link", ros::Time(0));
+
+    } catch (tf2::TransformException &ex) {
+        ROS_WARN("Pcl2process Get TF ERROR!");
+        return;
+    }
+    current_x = base2map.transform.translation.x;
+    current_y = base2map.transform.translation.y;
+
+    if((pcl2cloud->points.size()) == 0){
+        return;
+    }
+    else{
+        pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+        pcl::VoxelGrid<pcl::PointXYZ> filter;
+        filter.setInputCloud(pcl2cloud);
+        filter.setLeafSize(0.04f, 0.04f, 0.04f);
+        filter.filter(*pcl2cloud);
+        ne.setInputCloud(pcl2cloud);
+        pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>());
+        ne.setSearchMethod(tree);
+        //存储输出数据
+        pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
+        //ne.setRadiusSearch(0.03); //使用半径在查询点周围3厘米范围内的所有临近元素
+        ne.setKSearch(10); //使用最近的10个点
+        ne.compute(*cloud_normals);
+        long point_num = 0;
+        for (long i = 0; i <= pcl2cloud->points.size(); i = i + 1) {
+            float gradient = (pow(cloud_normals->points[i].normal_x, 2) + pow(cloud_normals->points[i].normal_y, 2)) / pow(cloud_normals->points[i].normal_z, 2);
+            if(gradient > 1.0f){
+                if(pcl2cloud->points[i].y > 6.6 or pcl2cloud->points[i].y < -6.6){
+                    continue;
+                }
+                if(pow(pcl2cloud->points[i].x - current_x, 2) + pow(pcl2cloud->points[i].y - current_y, 2) > 0.09){
+                    pcl2cloud->points[i].z = 0;
+                    pcl2cloud_out->points.push_back(pcl2cloud->points[i]);
+                    point_num = point_num + 1;
+                }
+            }
+        }
+
+        pcl::PointXYZ point4push;
+        for (float x = -7.00; x < 22.0; x = x + 0.05) {
+            point4push.x = x;
+            point4push.y = -7.85f;
+            point4push.z = 0.2f;
+            pcl2cloud_out->points.push_back(point4push);
+            point4push.y = 7.85f;
+            pcl2cloud_out->points.push_back(point4push);
+            point_num = point_num + 2;
+        }
+        for (float y = -7.85; y < 7.85; y = y + 0.05) {
+            point4push.x = -7.00f;
+            point4push.y = y;
+            point4push.z = 0.2f;
+            pcl2cloud_out->points.push_back(point4push);
+            point4push.x = 22.0f;
+            pcl2cloud_out->points.push_back(point4push);
+            point_num = point_num + 2;
+        }
+
+        pcl2cloud_out->width = point_num;
+        pcl2cloud_out->height = 1;
+        pcl2cloud_out->points.resize(pcl2cloud_out->width * pcl2cloud_out->height);
+        pcl::toROSMsg(*pcl2cloud_out, ROSPCL_output);
+        ROSPCL_output.header.frame_id = "map";
+        pcl_publisher.publish(ROSPCL_output);
+    }
+}
+
+void getcloud_vec(const sensor_msgs::PointCloud2ConstPtr &laserCloudMsg) {//法向量法投影转平面
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl2cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    sensor_msgs::PointCloud2 ROSPCL_output;
+    pcl::fromROSMsg(*laserCloudMsg, *pcl2cloud);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl2cloud_out(new pcl::PointCloud<pcl::PointXYZ>);
+
+    geometry_msgs::TransformStamped base2map;
+    try {
+        base2map = tfBuffer.lookupTransform("map", "base_link", ros::Time(0));
+
+    } catch (tf2::TransformException &ex) {
+        ROS_WARN("Pcl2process Get TF ERROR!");
+        return;
+    }
+    current_x = base2map.transform.translation.x;
+    current_y = base2map.transform.translation.y;
+
+    if((pcl2cloud->points.size()) == 0){
+        return;
+    }
+    else{
+        pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+        pcl::VoxelGrid<pcl::PointXYZ> filter;
+        filter.setInputCloud(pcl2cloud);
+        filter.setLeafSize(0.04f, 0.04f, 0.04f);
+        filter.filter(*pcl2cloud);
+        ne.setInputCloud(pcl2cloud);
+        pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>());
+        ne.setSearchMethod(tree);
+        //存储输出数据
+        pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
+        //ne.setRadiusSearch(0.03); //使用半径在查询点周围3厘米范围内的所有临近元素
+        ne.setKSearch(10); //使用最近的10个点
+        ne.compute(*cloud_normals);
+        long point_num = 0;
+        for (long i = 0; i <= pcl2cloud->points.size(); i = i + 1) {
+            float gradient = (pow(cloud_normals->points[i].normal_x, 2) + pow(cloud_normals->points[i].normal_y, 2)) / pow(cloud_normals->points[i].normal_z, 2);
+            if(gradient > 1.0f){
+                if(pcl2cloud->points[i].y > 6.6 or pcl2cloud->points[i].y < -6.6){
+                    continue;
+                }
+                if(pow(pcl2cloud->points[i].x - current_x, 2) + pow(pcl2cloud->points[i].y - current_y, 2) > 0.09){
+                    pcl2cloud->points[i].z = 0;
+                    pcl2cloud_out->points.push_back(pcl2cloud->points[i]);
+                    point_num = point_num + 1;
+                }
+            }
+        }
+
+        pcl::PointXYZ point4push;
+        for (float x = -7.00; x < 22.0; x = x + 0.05) {
+            point4push.x = x;
+            point4push.y = -7.85f;
+            point4push.z = 0.2f;
+            pcl2cloud_out->points.push_back(point4push);
+            point4push.y = 7.85f;
+            pcl2cloud_out->points.push_back(point4push);
+            point_num = point_num + 2;
+        }
+        for (float y = -7.85; y < 7.85; y = y + 0.05) {
+            point4push.x = -7.00f;
+            point4push.y = y;
+            point4push.z = 0.2f;
+            pcl2cloud_out->points.push_back(point4push);
+            point4push.x = 22.0f;
+            pcl2cloud_out->points.push_back(point4push);
+            point_num = point_num + 2;
+        }
+
+        pcl2cloud_out->width = point_num;
+        pcl2cloud_out->height = 1;
+        pcl2cloud_out->points.resize(pcl2cloud_out->width * pcl2cloud_out->height);
+        pcl::toROSMsg(*pcl2cloud_out, ROSPCL_output);
+        ROSPCL_output.header.frame_id = "map";
+        pcl_publisher.publish(ROSPCL_output);
+    }
+}
+
+void getcloud(const sensor_msgs::PointCloud2ConstPtr &laserCloudMsg) {//使用体素+梯度法进行点云分割
+    std::vector<std::vector<int>> point_list1(2800);
+    std::vector<PointInt> point_list2;
+    geometry_msgs::TransformStamped base2map;
+    try {
+        base2map = tfBuffer.lookupTransform("map", "base_link", ros::Time(0));
+
+    } catch (tf2::TransformException &ex) {
+        ROS_WARN("Pcl2process Get TF ERROR!");
+        return;
+    }
+    current_x = base2map.transform.translation.x;
+    current_y = base2map.transform.translation.y;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl2cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    sensor_msgs::PointCloud2 ROSPCL_output;
+    pcl::fromROSMsg(*laserCloudMsg, *pcl2cloud);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl2cloud_out(new pcl::PointCloud<pcl::PointXYZ>);
+    unsigned int point_num = 0;
+    cv::Mat hight_map(2801, 2801, CV_8UC1, cv::Scalar(0));
+    cv::Mat gradient_map(2801, 2801, CV_8UC1, cv::Scalar(0));
+    for (auto point: (pcl2cloud->points)) {
+        point.z = point.z + 0.52;
+        if (point.x < 22.4) {   //判断该点是否属于车身
+            if (point.x > -5.4) {
+                if (point.y < 13.8) {
+                    if (point.y > -13.8) {
+                        if (point.z > -1.2) {
+                            if (point.z < 1.2) {
+                                if (pow((current_x - point.x), 2) + pow((current_y - point.y), 2) < 0.067) {
+                                    continue;
+                                }
+                                /*
+                                float point_distance = sqrtf(point.x * point.x + point.y * point.y);
+                                if(point_distance > 0.4f){
+                                    if(float_abs(point.z) > 0.7f * (point_distance - 0.3f)){
+                                        pcl::PointXYZ point4push;
+                                        /*
+                                        point4push.x = point.x;
+                                        point4push.y = point.y;
+                                        point4push.z = 0.0f;
+
+                                        pcl2cloud_out->points.push_back(point);
+                                        point_num = point_num + 1;
+                                        continue;
+                                    }
+                                }
+                                */
+                                int px = 100 * point.x + 550;
+                                int py = 100 * point.y + 1400;
+                                unsigned char pz = 100 * point.z + 130;
+                                if (!(hight_map.at<uchar>(px, py))) {
+                                    hight_map.at<uchar>(px, py) = pz;
+                                    point_list1[px].push_back(py);
+                                    point_list2.push_back({px, py, pz});
+                                } else {
+                                    if (pz > hight_map.at<uchar>(px, py)) {
+                                        hight_map.at<uchar>(px, py) = pz;
+                                        point_list1[px].push_back(py);
+                                        point_list2.push_back({px, py, pz});
+                                    }
+                                }
+                            }
+                            //continue;
+                        }
+                    }
+                }
+            }
+        }
+        //pcl2cloud_out->points.push_back(point);
+    }
+
+    for (auto point: point_list2) {
+        unsigned char max = 0;
+        unsigned char min = 255;
+        if (point.p_x < 1435) {
+            if (point.p_x > 1365) {
+                if (point.p_y < 1435) {
+                    if (point.p_y > 1365) {
+                        continue;
+                    }
+                }
+            }
+        }
+        int surround_point_x = 0;
+        for (surround_point_x = ((point.p_x - 10) > 0 ? (point.p_x - 10) : 0);//在附近20*20cm范围内搜索高度差异最大的点
+             surround_point_x < ((point.p_x + 10) < 2799 ? (point.p_x + 10) : 2799);
+             surround_point_x = surround_point_x + 1) {
+            for (auto surround_point_y: point_list1[surround_point_x]) {
+                if ((surround_point_y < point.p_y + 10) and (surround_point_y > point.p_y - 10)) {
+                    if (hight_map.at<uchar>(surround_point_x, surround_point_y) < min) {
+                        min = hight_map.at<uchar>(surround_point_x, surround_point_y);
+                    }
+                    if (hight_map.at<uchar>(surround_point_x, surround_point_y) > max) {
+                        max = hight_map.at<uchar>(surround_point_x, surround_point_y);
+                    }
+                }
+                if (surround_point_y > point.p_y + 10) {
+                    break;
+                }
+            }
+        }
+        unsigned char point_gradient = max - min;
+        gradient_map.at<uchar>(point.p_x, point.p_y) = point_gradient;
+        if (point_gradient > 14) {
+            pcl::PointXYZ point4push;
+            point4push.x = (float) (point.p_x - 550) / 100;
+            point4push.y = (float) (point.p_y - 1400) / 100;
+            point4push.z = 0.15f;
+            pcl2cloud_out->points.push_back(point4push);
+            point_num = point_num + 1;
+        } else {
+            unsigned char max_5cm = 0;
+            unsigned char min_5cm = 255;
+            int surround_point_x = 0;
+            for (surround_point_x = ((point.p_x - 5) > 0 ? (point.p_x - 5) : 0);
+                 surround_point_x < ((point.p_x + 5) < 2799 ? (point.p_x + 5) : 2799);
+                 surround_point_x = surround_point_x + 1) {
+                for (auto surround_point_y: point_list1[surround_point_x]) {
+                    if ((surround_point_y < point.p_y + 5) and (surround_point_y > point.p_y - 5)) {
+                        if (hight_map.at<uchar>(surround_point_x, surround_point_y) < min) {
+                            min_5cm = hight_map.at<uchar>(surround_point_x, surround_point_y);
+                        }
+                        if (hight_map.at<uchar>(surround_point_x, surround_point_y) > max) {
+                            max_5cm = hight_map.at<uchar>(surround_point_x, surround_point_y);
+                        }
+                    }
+                    if (surround_point_y > point.p_y + 5) {
+                        break;
+                    }
+                }
+            }
+            unsigned char point_gradient_5cm = max_5cm - min_5cm;
+            //gradient_map.at<uchar>(point.p_x, point.p_y) = point_gradient;
+            if (point_gradient > 10) {
+                pcl::PointXYZ point4push;
+                point4push.x = (float) (point.p_x - 550) / 100;
+                point4push.y = (float) (point.p_y - 1400) / 100;
+                point4push.z = 0.12f;
+                pcl2cloud_out->points.push_back(point4push);
+                point_num = point_num + 1;
+            }
+        }
+    }
+//    pcl::PointXYZ point4push;
+//    for (float x = -5.88; x < 21.8; x = x + 0.05) {
+//        point4push.x = x;
+//        point4push.y = -7.51f;
+//        point4push.z = 0.2f;
+//        pcl2cloud_out->points.push_back(point4push);
+//        point4push.y = 7.24f;
+//        pcl2cloud_out->points.push_back(point4push);
+//        point_num = point_num + 2;
+//    }
+//    for (float y = -7.51; y < 7.24; y = y + 0.05) {
+//        point4push.x = -5.88f;
+//        point4push.y = y;
+//        point4push.z = 0.2f;
+//        pcl2cloud_out->points.push_back(point4push);
+//        point4push.x = 21.8f;
+//        pcl2cloud_out->points.push_back(point4push);
+//        point_num = point_num + 2;
+//    }
+    pcl2cloud_out->width = point_num;
+    pcl2cloud_out->height = 1;
+    pcl2cloud_out->points.resize(pcl2cloud_out->width * pcl2cloud_out->height);
+    cv::threshold(gradient_map, gradient_map, 10, 255, cv::THRESH_BINARY);
+    pcl::VoxelGrid<pcl::PointXYZ> filter;
+    filter.setInputCloud(pcl2cloud_out);
+    filter.setLeafSize(0.03f, 0.03f, 0.01f);//体素下采样
+    filter.filter(*pcl2cloud_out);
+
+    pcl::toROSMsg(*pcl2cloud_out, ROSPCL_output);
+//    cv::imshow("gradient_map", hight_map);
+//    cv::waitKey(1);
+    ROSPCL_output.header.frame_id = "map";
+    pcl_publisher.publish(ROSPCL_output);
+    //ROS_INFO("%d",pcl2cloud->points.size());
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "pointcloud_process");
+    ros::NodeHandle pnh("~");
+    tf2_ros::TransformListener tfListener(tfBuffer);
+    auto subCloud = pnh.subscribe<sensor_msgs::PointCloud2>("/pointcloud2_in", 1, getcloud_vec);
+    pcl_publisher = pnh.advertise<sensor_msgs::PointCloud2>("/pointcloud2_out", 1);
+    ros::spin();
+    return 0;
+    //auto sub1 = pnh.subscribe("/pointcloud2_in", 100, laserCallback);
+    //auto sub2 = pnh.subscribe("/depth_image", 100, depth_img_callback);
+    //pub = pnh.advertise<sensor_msgs::LaserScan>("/projected_scan", 1);
+    //cv::destroyWindow("depth");
+}
---
+++ project2plane/src/laserProjection.cpp
@@ -0,0 +1,117 @@
+//
+// Created by bismarck on 12/11/22.
+//
+#define USE_CUSTOM_LASER2SCAN
+
+#include <cmath>
+
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
+
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+ros::Publisher pub;
+bool publish_tf = false;
+
+bool callbacked_flag = false;
+tf2_ros::Buffer tfBuffer;
+
+void project2plane_callback(const ros::TimerEvent&){    //将3D位置投影到2D地图上用于导航
+    static tf2_ros::TransformBroadcaster br;
+    geometry_msgs::TransformStamped laser2base, base2map;
+    try {
+        base2map = tfBuffer.lookupTransform("map", "base_link", ros::Time(0));
+    }
+    catch (tf2::TransformException &ex) {
+        ROS_WARN("Project2Scan Get TF ERROR!");
+        return;
+    }
+
+    tf2::Quaternion b2m{base2map.transform.rotation.x, base2map.transform.rotation.y,
+                        base2map.transform.rotation.z, base2map.transform.rotation.w};
+    double roll = 0, pitch = 0, yaw = 0;
+    tf2::Matrix3x3(b2m).getRPY(roll, pitch, yaw);
+
+    if (publish_tf) {
+        geometry_msgs::TransformStamped trans = base2map;
+        tf2::Quaternion q;
+        q.setRPY(0, 0, yaw);
+        trans.child_frame_id = "plane_base_link";
+        trans.header.stamp = ros::Time::now();
+        trans.transform.rotation.x = q.x();
+        trans.transform.rotation.y = q.y();
+        trans.transform.rotation.z = q.z();
+        trans.transform.rotation.w = q.w();
+        trans.transform.translation.z = 0;
+        br.sendTransform(trans);
+    }
+}
+
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg) {   //根据当前姿态对线性激光结果进行转换
+    sensor_msgs::LaserScan new_msg = *msg;
+    static tf2_ros::TransformBroadcaster br;
+    geometry_msgs::TransformStamped laser2base, base2map;
+    try {
+        base2map = tfBuffer.lookupTransform("map", "base_link", ros::Time(0));
+    }
+    catch (tf2::TransformException &ex) {
+        ROS_WARN("Project2Scan Get TF ERROR!");
+        return;
+    }
+
+    tf2::Quaternion b2m{base2map.transform.rotation.x, base2map.transform.rotation.y,
+                        base2map.transform.rotation.z, base2map.transform.rotation.w};
+    double roll = 0, pitch = 0, yaw = 0;
+    tf2::Matrix3x3(b2m).getRPY(roll, pitch, yaw);
+    float theta = new_msg.angle_min;
+    for (auto &it: new_msg.ranges) {
+        it = it * cos(pitch*cos(theta));
+        theta = theta + new_msg.angle_increment;
+    }
+    //ROS_INFO("Start Angle: %f, End Angle: %f", new_msg.angle_min, new_msg.angle_max);
+    new_msg.header.frame_id = "plane_" + new_msg.header.frame_id;
+    for(int i = 0; i<10; i=i+1){
+        new_msg.ranges[i]=0.0;
+    }
+    for(int i = 631; i<640; i=i+1){
+        new_msg.ranges[i]=0.0;
+    }
+    pub.publish(new_msg);
+
+}
+
+void depth_img_callback(const sensor_msgs::ImageConstPtr& msg){
+    cv::Mat cv_img0 =  cv_bridge::toCvCopy(msg, "16UC1")->image;
+    cv::imshow("depth", cv_img0);
+    cv::waitKey(1);
+    /*
+    sensor_msgs::LaserScan new_msg;
+    。。。处理深度图到扫瞄。。。
+    。。。投影转平面。。。
+    */
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "laser_projection");
+    ros::NodeHandle pnh("~");
+    pnh.param<bool>("publish_tf", publish_tf, false);
+    tf2_ros::TransformListener tfListener(tfBuffer);
+//#ifndef USE_CUSTOM_LASER2SCAN
+    auto sub1 = pnh.subscribe("/scan", 100, laserCallback);
+    ros::Timer timer1 = pnh.createTimer(ros::Duration(0.02), project2plane_callback);
+//    auto odomSub = pnh.subscribe<nav_msgs::Odometry>("/Odometry", 10, lidar_to_odom);
+//#else
+    //auto sub2 = pnh.subscribe("/depth_image", 100, depth_img_callback);
+//#endif
+    pub = pnh.advertise<sensor_msgs::LaserScan>("/projected_scan", 1);
+    ros::spin();
+//    cv::destroyWindow("depth");
+    return 0;
+}
